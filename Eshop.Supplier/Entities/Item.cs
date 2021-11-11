4b99bac57fa23ea9ae62b6af5507c1f9fb8b54c2
Abhijeet Kumar <44672451+Abhijeet05061998@users.noreply.github.com>
2021-11-11 10:30:26 +0530

    Update Item.cs

--- Eshop.Supplier/Entities/Item.cs
+++ Eshop.Supplier/Entities/Item.cs
@@ -9,12 +9,13 @@ namespace Eshop.Supplier.Entities
     [Table("Item")]
     public class Item
     {
+        //primary key
         [Key]
         [StringLength(5)]
         [Column(TypeName ="Char")]
         public string ItemId { get; set; }
         [StringLength(20)]
-        [Required] //set as not null
+        [Required] //set as not null (value would not be null if you set as requared)
         public string ItemName { get; set; }
         public int Price { get; set; }
         public int? Stock { get; set; } //set as null
